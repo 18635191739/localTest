8327cfd406ba0a0c654bb56165aa58f27b4330ca
18635191739 <83003599@qq.com>
2018-08-03 02:35:39 +0900

    A TEST PROJ
    TEST FROM HOME

---
+++ src/com/WriteInHome/D0803.java
@@ -0,0 +1,5 @@
+package com.WriteInHome;
+
+public class D0803 {
+    //JUST Write this for test
+}
---
+++ src/com/WriteInHome/Mainfunc.java
@@ -0,0 +1,7 @@
+package com.WriteInHome;
+
+public class Mainfunc {
+    public static void main(String[] args){
+        System.out.println("Hello GitHub");
+    }
+}
